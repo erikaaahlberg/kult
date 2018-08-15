2eef34972d0c6b9b022b86b8b0c202ec720b0f4e
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-15 17:37:40 +0200

    Fetch and display bookings from server

--- src/components/routes/Book.js
+++ src/components/routes/Book.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default function Book(){
-  return(
-    <div>
-      <h1>Boka bord</h1>
-      <p>Det här är komponenten för bokningssidan – jag finns i components/routes/Book.</p>
-    </div>
-  )
+export default class Book extends Component{
+
+  state = {
+    existingBookings: [],
+  }
+
+  fetchBookings = () => {
+    fetch('/api/bookings')
+    .then(response => response.json())
+    .then((existingBookings) => {
+        this.setState({existingBookings})
+    })
+    .catch((error) => {
+      // TODO: Handle error output to user, remove console.log
+        console.log(error);
+    });
+  }
+
+  /** TODO:
+   * This is a temporary (!) render to see what raw data comes out of db,
+   * this should not be shown here later. And maybe fetch elsewhere.
+   * */
+  renderExistingBookings = () => {
+    if(!this.state.existingBookings){
+      return;
+    } else {
+      return this.state.existingBookings.map((booking) => {
+        return (
+          <ul key={booking.id}>
+            <h3>Bokning: </h3>
+            <li>Datum: {booking.date}</li>
+            <li>Antal personer: {booking.guests}</li>
+            <li>Sittning: {booking.session}</li>
+          </ul>
+        )
+      }
+    )}
+  }
+
+  componentDidMount(){
+    this.fetchBookings();
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Boka bord</h1>
+        <p>Det här är komponenten för bokningssidan – jag finns i components/routes/Book.</p>
+        <h2>Nuvarande bokningar i databasen:</h2>
+        {!this.state.existingBookings &&
+          <p>Inga bokningar just nu.</p>
+        }
+        {this.renderExistingBookings()}
+      </div>
+    )
+  }
 };
