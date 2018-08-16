a040078484a1fce6ce390753b659a56a94bb8323
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-16 22:29:25 +0200

    Turn datepicker into seperate calendar-component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,60 +1,38 @@
-import React, {Component} from "react";
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import React from "react";
 import 'react-datepicker/dist/react-datepicker.css';
+import Calendar from "./Calendar";
 
-export default class BookingForm extends Component{
-  state = {
-    startDate: moment(),
-  }
-
-  constructor (props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
- 
-  handleChange(date) {
-    this.setState({
-      startDate: date,
-    });
-  }
-
-  render(){
-    return(
-      <form action="/api/create_booking/" method="POST">
-        <label htmlFor="create_guest">Antal gäster</label>
-        <br/>
-        <input placeholder="Max 4" name="create_guests"/>
-        <br/>
-        <label htmlFor="create_session">Sittning</label>
-        <br/>
-        <select name="create_session">
-          <option value="18:00">18:00</option>
-          <option value="21:00">21:00</option>
-        </select>
-        <br/>
-        <label htmlFor="create_date">Välj datum</label>
-        <br/>
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleChange} 
-          name="create_date"
-        />
-        <br/>
-        <label htmlFor="create_date">Namn</label>
-        <br/>
-        <input placeholder="Kult Svensson" name="create_name"/>
-        <br/>
-        <label htmlFor="create_date">E-mail</label>
-        <br/>
-        <input placeholder="kult@gmail.com" name="create_email"/>
-        <br/>
-        <label htmlFor="create_date">Phone</label>
-        <br/>
-        <input placeholder="+46 761234567" name="create_phone" required/>
-        <br/>
-        <button>Boka</button>
-      </form>
-    )
-  }
+export default function BookingForm(){
+  return(
+    <form action="/api/create_booking/" method="POST">
+      <label htmlFor="create_guest">Antal gäster</label>
+      <br/>
+      <input placeholder="Max 4" name="create_guests"/>
+      <br/>
+      <label htmlFor="create_session">Sittning</label>
+      <br/>
+      <select name="create_session">
+        <option value="18:00">18:00</option>
+        <option value="21:00">21:00</option>
+      </select>
+      <br/>
+      <label htmlFor="create_date">Välj datum</label>
+      <br/>
+      <Calendar/>
+      <br/>
+      <label htmlFor="create_date">Namn</label>
+      <br/>
+      <input placeholder="Kult Svensson" name="create_name"/>
+      <br/>
+      <label htmlFor="create_date">E-mail</label>
+      <br/>
+      <input placeholder="kult@gmail.com" name="create_email"/>
+      <br/>
+      <label htmlFor="create_date">Phone</label>
+      <br/>
+      <input placeholder="+46 761234567" name="create_phone" required/>
+      <br/>
+      <button>Boka</button>
+    </form>
+  )
 };
