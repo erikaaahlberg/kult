65fe2515325068f24b43a458318a8049599e7fc7
unknown <erika.ahlberg@medieinstitutet.se>
2018-08-17 11:40:49 +0200

    pulled new master

---
+++ src/components/input/DatePicker.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+/*import * as components from './grid';*/
+
+import Container from '../grid/Container';
+import Row from '../grid/Row';
+import Div from '../grid/Div';
+
+
+class DatePicker extends Component {
+    state = {
+        takenDates: []
+    }
+    componentDidMount(){
+        this.fetchBookings();
+        this.countBookings();
+    }
+
+    fetchBookings = () => {
+        return fetch('api/bookings')
+        .then((response) => response.json())
+            .then((fetchedBookings) => {
+                //this.setState({ takenDates: fetchedBookings })
+                return fetchedBookings;
+            })
+            .catch((error) => {
+              // TODO: Handle error output to user, remove console.log
+                console.log(error);
+            });
+    }
+
+    countBookings = () => {
+        let numberOfBookings = 0;
+        let fullyBooked = [];
+        this.fetchBookings()
+            .then((fetchedBookings) => {
+                let p = 1;
+                for (let i = 0; i < fetchedBookings.length; i++) {
+                    if (fetchedBookings[i].date === fetchedBookings[p].date && fetchedBookings[i].session === fetchedBookings[p].session) {
+                        numberOfBookings += 1;
+                        if (numberOfBookings === 10) {
+                            fullyBooked.push({
+                                date: fetchedBookings[i].date,
+                                session: fetchedBookings[i].session
+                            });
+                        }
+                        if(p < fetchedBookings.length) {
+                            p += 1;
+                        }
+                    }
+                    console.log(numberOfBookings);
+                    console.log(fetchedBookings[i]);
+                }
+                /*
+                const keys = Object.keys(fetchedBookings);
+                for (let key of keys) {
+                    console.log(fetchedBookings[key]); /*
+                    if(fetchedBookings[key].date === bookings.date && fetchedBookings.session === bookings.session) {
+                        console.log('hej');
+                    } 
+                }*/
+            })
+    }
+    
+   
+    render () {
+        return (
+            <input type = "date"/>
+        )
+    }
+
+}
+
+export default DatePicker;
+/*id = { props.id } className = { props.styleClass } onClick = { props.event } */
