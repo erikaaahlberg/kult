11b1de8a4d52c2c37615bc88237e3246a3489fe6
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-17 14:42:56 +0200

    Add draft for editing bookings

--- src/components/routes/Admin.js
+++ src/components/routes/Admin.js
@@ -1,17 +1,34 @@
 import React, {Component} from "react";
+import moment from 'moment';
 import Calendar from "../Calendar";
 
 export default class Admin extends Component{
 
   state = {
     bookingsOnSelectedDate: [],
+    selectedIdToEdit: null,
   }
 
-  renderExistingBookings = () => {
+  renderBookings = () => {
     if(!this.state.bookingsOnSelectedDate){
       return;
-    } else {
+    }
+     else {
       return this.state.bookingsOnSelectedDate.map((booking) => {
+        if(this.state.selectedIdToEdit === booking.id){
+          return (
+            <ul key={booking.id}>
+            <h3>REDIGERAR: </h3>
+            <li>Datum: {booking.date}</li>
+            <li>Antal personer: {booking.guests}</li>
+            <li>Sittning: {booking.session}</li>
+            <li>Bokat av: {booking.name}</li>
+            <li>E-mail: {booking.email}</li>
+            <li>Telefon: {booking.phone}</li>
+            <button onClick={() => {this.setState({selectedIdToEdit: null})}}>Klar</button>
+          </ul>
+          )
+        }
         return (
           <ul key={booking.id}>
             <h3>Bokning: </h3>
@@ -21,6 +38,7 @@ export default class Admin extends Component{
             <li>Bokat av: {booking.name}</li>
             <li>E-mail: {booking.email}</li>
             <li>Telefon: {booking.phone}</li>
+            <button onClick={() => {this.setState({selectedIdToEdit: booking.id})}}>Redigera</button>
           </ul>
         )
       }
