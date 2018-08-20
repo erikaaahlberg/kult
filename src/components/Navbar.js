54ad3dc505995ab6b25af9c6c34b2f66e75aac8c
vctrklndr <victor@nasa.se>
2018-08-20 14:50:26 +0200

    Add styling to home and add a toggle hamburger bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar(){
   return(
-    <ul>
+    <ul className="navbar">
       <li>
         <Link to="/">Hem</Link>
       </li>
--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
+import "../styles/Navbar.css";
+import "../styles/Keyframes.css";
+import CloseIcon from '../images/close.svg';
 
-export default function Home(){
-  return(
-    <div>
-      <h1>KULT</h1>
-      <p>Det här är komponenten för landingssidan – jag finns i components/routes/Home.</p>
-    </div>
-  )
+export default class Home extends Component {
+  state = {
+    toggle: "",
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      toggle: "open",
+    })
+
+    if(this.state.toggle === "open") {
+      this.setState({
+        toggle: "",
+      })
+    }
+  }
+  
+  render(){
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <div id="navIcon" className={this.state.toggle} onClick={this.toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <h1>KULT</h1>
+          <p>Det här är komponenten för landingssidan – jag finns i components/routes/Home.</p>
+        </div>
+        <div className="gdpr">
+          <img className="closeIcon" src={CloseIcon} alt="Close GDPR" />
+          <p>
+            Casper nose grab aerial 1080 Danny Way. Nosegrind pogo boned out indy grab. Air
+            rail Stacy Peralta hand rail hardware. Kevin Harris drop in lip fastplant. Craig
+            Patterson speed wobbles judo air blunt Tracker. Grab 540 baseplate nosegrind. 
+            Bruised heel nose bump ho-ho vert. Cess slide bluntslide kidney Mike York skater.
+            Rad 900 Primo slide helipop Powell Peralta.
+          </p>
+        </div>
+      </React.Fragment>
+    )
+  }
 };
