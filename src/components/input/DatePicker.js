eb799f47601fb1be2be7f9a3ae78e0febdd981e9
unknown <erika.ahlberg@medieinstitutet.se>
2018-08-20 12:31:19 +0200

    functions for sorting arrays based on seats available and fully booked dates

--- src/components/input/DatePicker.js
+++ src/components/input/DatePicker.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class DatePicker extends Component {
     state = {
-        availableSessions: []
+        availableSessions: [],
+        fullyBookedSessions: [],
+        fullyBookedDates: ''
     }
     componentDidMount(){
         this.fetchBookings();
-        this.countBookings();
+        this.sortBySession();
     }
 
     fetchBookings = () => {
@@ -24,18 +26,27 @@ class DatePicker extends Component {
             });
     }
 
-    countBookings = () => {
+    sortBySession = () => {
         let numberOfBookings = 0;
         let availableSessions = [];
+        let fullyBookedSessions = [];
         this.fetchBookings()
             .then((fetchedBookings) => {
                 for (let i = 0; i < fetchedBookings.length; i++) {
+                    let numberOfBookings = fetchedBookings[i].count;
                     console.log(fetchedBookings[i].count);
-                    if (fetchedBookings[i].count < 10) {
-                        let numberOfBookings = fetchedBookings[i].count;
-                        const tablesLeft = 10 - numberOfBookings;
+                    if (numberOfBookings < 5) {
+                        const tablesLeft = 5 - numberOfBookings;
 
                         availableSessions.push({
+                            date: fetchedBookings[i].date,
+                            session: fetchedBookings[i].session,
+                            tablesLeft: tablesLeft
+                        });
+                    }
+                    else if (numberOfBookings === 5) {
+                        const tablesLeft = 5 - numberOfBookings;
+                        fullyBookedSessions.push({
                             date: fetchedBookings[i].date,
                             session: fetchedBookings[i].session,
                             tablesLeft: tablesLeft
@@ -47,27 +58,36 @@ class DatePicker extends Component {
                     console.log(availableSessions);
                     this.setState({ availableSessions: availableSessions });
                 }
+                if (fullyBookedSessions.length > 0) {
+                    console.log(fullyBookedSessions);
+                    this.setState({ fullyBookedSessions: fullyBookedSessions });
+                }
+                
+        this.countFullyBookedDates();
             });
     }
     
-    countFullyBooked = () => {
+    countFullyBookedDates = () => {
+        let fullyBookedSessions = this.state.fullyBookedSessions;
         let fullyBookedDates = [];
-        let firstSession = 0;
-        let secondSession = 0;
-        for (let i = 0; i < this.state.fullyBookedSessions.length; i++) {
-            if (this.state.fullyBookedSessions[i].session === 18.00){
-                firstSession += 1;
-            }
-            else if (this.state.fullyBookedSessions[i].session === 21.00) {
-                secondSession += 1;
-            }
-            if (firstSession === 10 && secondSession === 10) {
-                fullyBookedDates.push(this.state.fullyBookedSessions[i].date);
+        const lastIndex = fullyBookedSessions.length -1;
+        console.log(fullyBookedSessions);
+        /*let firstSession = 0;
+        let secondSession = 0;*/
+        
+        for (let i = 0; i < fullyBookedSessions.length; i++) {
+            if (i != lastIndex) {
+                for (let p = i + 1; p < fullyBookedSessions.length; p++) {
+                    if (fullyBookedSessions[i].date === fullyBookedSessions[p].date) {
+                        fullyBookedDates.push(fullyBookedSessions[i].date);
+                    }
+                }
             }
         }
-        this.setState({ fullyBookedDates : fullyBookedDates });
-        console.log(this.state.fullyBookedSessions);
-        console.log(this.state.fullyBookedDates);
+        if(fullyBookedDates.length > 0) {
+            this.setState({ fullyBookedDates: fullyBookedDates });
+            console.log(this.state.fullyBookedDates);
+        }
     }
 
     render () {
@@ -124,5 +144,7 @@ export default DatePicker;
         this.setState({ fullyBookedDates : fullyBookedDates });
         console.log(this.state.fullyBookedSessions);
         console.log(this.state.fullyBookedDates);
+
+        fullyBookedDates = fullyBookedSessions.filter(sessions => sessions.date === fullyBookedSessions[i].date); 
     }
     */
