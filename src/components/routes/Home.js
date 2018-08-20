7530e2d9113bcd7f8914d1ddf758fd21e4465cdf
vctrklndr <victor@nasa.se>
2018-08-20 13:31:33 +0200

    Update calendar styling and add fonts

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home(){
   return(
     <div>
-      <h1>Hem</h1>
+      <h1>KULT</h1>
       <p>Det här är komponenten för landingssidan – jag finns i components/routes/Home.</p>
     </div>
   )
