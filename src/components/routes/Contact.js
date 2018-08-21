6e38a7f8f4fa36ab71d755efd0901cb6cd54427e
vctrklndr <victor@nasa.se>
2018-08-21 19:19:19 +0200

    Update styling for Contact.js

--- src/components/routes/Contact.js
+++ src/components/routes/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact(){
   return(
     <div className="wrapper">
-      <h1>KONTAKT</h1>
+      <h1 id="contactHeader">KONTAKT</h1>
       <p>hej@kultrestaurang.se</p>
     </div>
   )
