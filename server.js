9569e965250d1654c562048b0b8b527f9b0bc352
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-22 17:04:24 +0200

    Change order of endpoints

--- server.js
+++ server.js
@@ -16,54 +16,31 @@ app.listen(3001, () => {
   console.log('Listening to port 3001!');
 })
 
-app.post('/api/create_booking', (req, res) => {
-  const guests = req.body.guests;
-  const date = req.body.date;
-  const session = req.body.session;
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
+app.get('/api/bookings', (req, res) => {
+  const queryString = "SELECT * FROM bookings";
 
-  const queryString =
-    `INSERT INTO bookings
-    (id, guests, date, session, name, email, phone)
-    VALUES ('', ?, ?, ?, ?, ? ,?)`;
-
-  connection.query(queryString, [guests, date, session, name, email, phone], (err, results, fields) => {
-    if(err){
-      console.log('Failed to add booking: ' + err);
-      res.sendStatus(500)
-      res.end();
-      return;
-    }
-  })
-});
-
-app.put('/api/update_booking', (req, res) => {
-  const date = req.body.date;
-  const guests = req.body.guests;
-  const session = req.body.session;
-  const name = req.body.name;
-  const email = req.body.email;
-  const phone = req.body.phone;
-  const id = req.body.id;
-
-  const queryString =
-    `UPDATE bookings
-    SET guests= ?, date= ?, session=?, name=?, email=?, phone= ?
-    WHERE id = ?`
-
-  connection.query(queryString, [guests, date, session, name, email, phone, id], (err, results, fields) => {
-    if(err){
-      console.log('Failed to add booking: ' + err);
+  connection.query(queryString, (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to get all bookings ' + err);
       res.sendStatus(500) // Show user internal server error
       res.end();
       return;
     }
-  })
 
-  res.end();
-});
+    const bookings = rows.map((row) => {
+      return {
+        id: row.id,
+        guests: row.guests,
+        date: row.date,
+        session: row.session,
+        name: row.name,
+        email: row.email,
+        phone: row.phone,
+      }
+    })
+    res.json(bookings)
+  })
+})
 
 app.get('/api/bookings/date/:date', (req, res) => {
   const queryString = "SELECT * FROM bookings WHERE date = ?";
@@ -92,29 +69,51 @@ app.get('/api/bookings/date/:date', (req, res) => {
   })
 })
 
-app.get('/api/bookings', (req, res) => {
-  const queryString = "SELECT * FROM bookings";
+app.post('/api/create_booking', (req, res) => {
+  const guests = req.body.guests;
+  const date = req.body.date;
+  const session = req.body.session;
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
 
-  connection.query(queryString, (err, rows, fields) => {
-    if (err) {
-      console.log('Failed to get all bookings ' + err);
+  const queryString =
+    `INSERT INTO bookings
+    (id, guests, date, session, name, email, phone)
+    VALUES ('', ?, ?, ?, ?, ? ,?)`;
+
+  connection.query(queryString, [guests, date, session, name, email, phone], (err, results, fields) => {
+    if(err){
+      console.log('Failed to add booking: ' + err);
+      res.sendStatus(500)
+      res.end();
+      return;
+    }
+  })
+  res.end();
+});
+
+app.put('/api/update_booking', (req, res) => {
+  const date = req.body.date;
+  const guests = req.body.guests;
+  const session = req.body.session;
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const id = req.body.id;
+
+  const queryString =
+    `UPDATE bookings
+    SET guests= ?, date= ?, session=?, name=?, email=?, phone= ?
+    WHERE id = ?`
+
+  connection.query(queryString, [guests, date, session, name, email, phone, id], (err, results, fields) => {
+    if(err){
+      console.log('Failed to add booking: ' + err);
       res.sendStatus(500) // Show user internal server error
       res.end();
       return;
     }
-
-    const bookings = rows.map((row) => {
-      return {
-        id: row.id,
-        guests: row.guests,
-        date: row.date,
-        session: row.session,
-        name: row.name,
-        email: row.email,
-        phone: row.phone,
-      }
-    })
-    res.json(bookings)
   })
-})
-
+  res.end();
+});
