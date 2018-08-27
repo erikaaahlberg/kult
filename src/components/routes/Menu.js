7f7f72828d50aaa87674b5c08efc2ccf2002a48b
Ola Löwbeer <lowbeer.ola@gmail.com>
2018-08-27 14:36:25 +0200

    react linkstim

--- src/components/routes/Menu.js
+++ src/components/routes/Menu.js
@@ -19,16 +19,16 @@ export default function Menu(){
       <p className="text-align-center">MACKEREL - FOIE GRAS - CHANTERELLE</p>
       <p className="text-align-center">DUCK - TRUFFLE - CELERIAC</p>
       <p className="text-align-center">RASPBERRY - WHITE CHOCOLATE - SPRUCE&nbsp;</p>
-        <p class="text-align-center">&nbsp;LEMON VERBENA - SALT - OLIVE OIL</p>
-          <p class="text-align-center"> </p>
-          <p class="text-align-center">&nbsp;755 SEK</p>
+      <p class="text-align-center">&nbsp;LEMON VERBENA - SALT - OLIVE OIL</p>
+      <p class="text-align-center"> </p>
+      <p class="text-align-center">&nbsp;755 SEK</p>
       <div className="buttonlink">      
       <Link to="/book">
       <button className="button">BOKA BORD</button>
       </Link>
-     <p className="text-align-center">&nbsp;+46 7283941</p>
+      <p className="text-align-center">&nbsp;+46 7283941</p>
 
- </div>
+    </div>
   </div>
   <br />
   </div>
