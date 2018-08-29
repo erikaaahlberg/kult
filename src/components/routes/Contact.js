3b5b2e9885f096d00ef628b06dfb7f381657dada
vctrklndr <victor@nasa.se>
2018-08-29 08:44:31 +0200

    Add Contact.css

--- src/components/routes/Contact.js
+++ src/components/routes/Contact.js
@@ -1,52 +1,51 @@
 import React from "react";
-import "../../assets/styles/Booking.css";
-
+import "../../assets/styles/Contact.css";
 
 export default function Contact(){
   return(
     <div className="wrapper">
       <h1 className="smallHeader">KONTAKT</h1>
-      <p>hej@kultrestaurang.se</p>
-
-      <div className="contactUsWrapper">
-        <p>
-          Kontakta oss gärna!
-        </p>
+      <p>
+        Kontakta oss gärna!
+      </p>
+        <div className="contactUsWrapper">
         <label htmlFor="name">Namn*</label>
         <br />
-        <input type="text"
-         placeholder="Namn"
-         id="contactName"
+        <input
+          type="text"
+          placeholder="Namn"
+          id="contactName"
          />
         <div className="phoneEmail">
-        <div className="phone">
-        <label htmlFor="name">Telefon*</label>
-        <br />
-        <input 
-        type="text" 
-        placeholder="Telefon"
-        id="contactPhone"
-        />
+          <div className="phone">
+            <label htmlFor="name">Telefon*</label>
+            <br />
+            <input 
+              type="text" 
+              placeholder="Telefon"
+              id="contactPhone"
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="name">Email*</label>
+            <br />
+            <input
+              type="text" 
+              placeholder="Email"
+              id="contactEmail"
+            />
+          </div>
         </div>
-         <div className="email">
-         <label htmlFor="name">Email*</label>
-        <br />
-        <input type="text" 
-        placeholder="Email"
-        id="contactEmail"
-        />
-        </div>
-      </div>
-         <div className="contactMessage">
-         <label htmlFor="name">Meddelande*</label>
-        <br />
-        <textarea  
-        placeholder="Meddelande"
-        id="contactMessage"
-        />
+        <div className="contactMessage">
+          <label htmlFor="name">Meddelande*</label>
+          <br />
+          <textarea  
+            placeholder="Meddelande"
+            id="contactMessage"
+          />
          </div>
-    </div>
-         <button className="button" type="submit">SKICKA</button>
+        </div>
+        <button className="button" type="submit">SKICKA</button>
       </div>
   )
 };
