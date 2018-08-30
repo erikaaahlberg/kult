4bd3af39541642a0021af9c8374f2270471d1a16
Emma Uddesson <emma.uddesson@gmail.com>
2018-08-30 10:11:19 +0200

    Add form and requirements

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -9,101 +9,106 @@ import Calendar from "./Calendar";
 import Image from "./Image";
 
 export default function BookingForm(props){
-  const { 
+  const {
     createNewBooking,
     updateBooking,
     updateDate,
     findSessionsForSelectedDate,
     availableSessions,
-    fullyBookedDates 
+    fullyBookedDates
   } = props;
 
   return(
     <React.Fragment>
-      <div className="bookingWrapper">
-        <div className="container">
-          <Image
-            className="icons"
-            src={ CalendarIcon }
-            alt="Calendar"
-          />
-          <div>
-            <label htmlFor="create_date">
-              Välj datum
-            </label>
-            <Calendar
-              updateDate={ updateDate }
-              fullyBookedDates={ fullyBookedDates }
-              findSessionsForSelectedDate={ findSessionsForSelectedDate }
+      <form>
+        <div className="bookingWrapper">
+          <div className="container">
+            <Image
+              className="icons"
+              src={ CalendarIcon }
+              alt="Calendar"
             />
+            <div>
+              <label htmlFor="create_date">
+                Välj datum
+              </label>
+              <Calendar
+                updateDate={ updateDate }
+                fullyBookedDates={ fullyBookedDates }
+                findSessionsForSelectedDate={ findSessionsForSelectedDate }
+              />
+            </div>
           </div>
-        </div>
-        <div className="container">
-          <Image
-            className="icons"
-            src={ ClockIcon }
-            alt="Clock"
-          />
-          <div>
-            <label htmlFor="create_session">
-              Sittning
-            </label>
-            <br />
-            <Selector
-              name={'create_session'}
-              availableSessions={ availableSessions }
-              updateBooking={ updateBooking }
+          <div className="container">
+            <Image
+              className="icons"
+              src={ ClockIcon }
+              alt="Clock"
             />
+            <div>
+              <label htmlFor="create_session">
+                Sittning
+              </label>
+              <br />
+              <Selector
+                name={'create_session'}
+                availableSessions={ availableSessions }
+                updateBooking={ updateBooking }
+              />
+            </div>
+          </div>
+          <div className="container">
+            <Image
+              className="icons"
+              src={ UserIcon }
+              alt="User"
+            />
+            <div>
+              <label htmlFor="create_guests">
+                Antal personer
+              </label>
+              <br />
+              <select name="create_guests" onChange={ updateBooking }>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
           </div>
         </div>
-        <div className="container">
-          <Image
-            className="icons"
-            src={ UserIcon }
-            alt="User"
+        <div className="contactInfoWrapper">
+          <p>
+            Innan din bokning kan genomföras – behöver
+            <br />vi veta lite mer om dig!
+          </p>
+          <label htmlFor="create_name">Namn</label>
+          <input
+            placeholder="Kult Svensson"
+            onChange={ updateBooking }
+            name="create_name"
+            id="create_name"
+            required
           />
-          <div>
-            <label htmlFor="create_guests">
-              Antal personer
-            </label>
-            <br />
-            <select name="create_guests" onChange={ updateBooking }>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-          </div>
+          <label htmlFor="create_email">E-mail</label>
+          <input
+            placeholder="kult@gmail.com"
+            onChange={ updateBooking }
+            name="create_email"
+            id="create_email"
+            required
+          />
+          <label htmlFor="create_phone">Telefon</label>
+          <input
+            placeholder="+46 761234567"
+            onChange={ updateBooking }
+            name="create_phone"
+            id="create_phone"
+            required
+          />
+          <button className="button" onClick={() => {createNewBooking}} type="submit">Boka</button>
         </div>
-      </div>
-      <div className="contactInfoWrapper">
-        <p>
-          Innan din bokning kan genomföras – behöver
-          <br />vi veta lite mer om dig!
-        </p>
-        <label htmlFor="create_name">Namn</label>
-        <input
-          placeholder="Kult Svensson"
-          onChange={ updateBooking }
-          name="create_name"
-          id="create_name"
-        />
-        <label htmlFor="create_email">E-mail</label>
-        <input
-          placeholder="kult@gmail.com"
-          onChange={ updateBooking }
-          name="create_email"
-          id="create_email"
-        />
-        <label htmlFor="create_phone">Telefon</label>
-        <input
-          placeholder="+46 761234567"
-          onChange={ updateBooking }
-          name="create_phone"
-          id="create_phone"
-        />
-        <button className="button" onClick={createNewBooking} type="submit">Boka</button>
-      </div>
+      </form>
     </React.Fragment>
   )
 }
