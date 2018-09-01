e8a6390fd345c717467e969c811231b7b1245d29
vctrklndr <victor@nasa.se>
2018-09-01 21:17:04 +0200

    Indenting fix

--- src/components/Gdpr.js
+++ src/components/Gdpr.js
@@ -4,29 +4,29 @@ import Image from "./Image";
 import CloseIcon from "../assets/images/close.svg";
 
 export default class Gdpr extends Component {
-    state = {
-      gdpr: "gdprVisible",
-    }
+  state = {
+    gdpr: "gdprVisible",
+  }
 
-    removeGDPR = () => {
-      this.setState({
-        gdpr: "gdprHidden",
-      });
-    }
+  removeGDPR = () => {
+    this.setState({
+      gdpr: "gdprHidden",
+    });
+  }
 
-    render() {
-      return (
-        <div className={this.state.gdpr}>
-          <Image className="closeIcon" src={CloseIcon} alt="Close GDPR" handleChange={this.removeGDPR} />
-          <p>
-              Genom att fortsätta på denna sida accepterar du vår integritetspolicy
-              och villkoren som ingår i den. Du tillåter oss även att lagra den
-              informationen du ger oss vid bokningar och direkt kontakt.
-              Du kan läsa mer om detta under integritetspolicy eller villkor.
-          </p>
-          <p>För frågor når du oss via vår kontaktsida.</p>
-          <p>Ha så kult på restaurangen!</p>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className={this.state.gdpr}>
+        <Image className="closeIcon" src={CloseIcon} alt="Close GDPR" handleChange={this.removeGDPR} />
+        <p>
+            Genom att fortsätta på denna sida accepterar du vår integritetspolicy
+            och villkoren som ingår i den. Du tillåter oss även att lagra den
+            informationen du ger oss vid bokningar och direkt kontakt.
+            Du kan läsa mer om detta under integritetspolicy eller villkor.
+        </p>
+        <p>För frågor når du oss via vår kontaktsida.</p>
+        <p>Ha så kult på restaurangen!</p>
+      </div>
+    );
+  }
 }
