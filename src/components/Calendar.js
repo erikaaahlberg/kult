af60b510173badda0a1b4d6dae8a6f2464c18d93
Emma Uddesson <emma.uddesson@gmail.com>
2018-09-04 17:25:30 +0200

    Update import

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import moment from "moment";
-import { formatDateString, transformDatesToMoment } from "./services/helpers";
+import { formatDateString, transformDatesToMoment } from "../services/helpers";
 import "moment/locale/sv";
 import "react-datepicker/dist/react-datepicker.css";
 import "../assets/styles/Datepicker.css";
